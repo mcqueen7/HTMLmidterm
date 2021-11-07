658685b8aa468c9a2055eabfbb1ade11596233b0
mcqueen <chqueen.tw@gmail.com>
2021-11-07 16:41:18 +0800

    slidebar commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.nav_link_now').parent().css('background-color',"rgba(192, 37, 231, 0.705)");
+
+});
+
+$(function() {
+    $(".flexslider").flexslider({
+    slideshowSpeed: 3700, //展示时间间隔ms
+    animationSpeed: 500, //滚动时间ms
+    touch: true //是否支持触屏滑动
+  });
+}); 
---
+++ js/news.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.nav_link_now').parent().css('background-color',"rgba(192, 37, 231, 0.705)");
+
+});
